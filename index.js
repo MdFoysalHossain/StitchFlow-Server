b28285f5c41afbc9a8fff37437aeefe2d3950606
MdFoysalHossain <imfoysl@gmail.com>
2025-12-15 20:58:09 +0600

    Vercel Push test 2

--- index.js
+++ index.js
@@ -10,9 +10,22 @@ app.use(express.json())
 const admin = require("firebase-admin");
 
 const serviceAccount = require("./stitchflow-firebase-adminsdk.json");
+const serverAccountInf = {
+    "type": process.env.FIREBASE_type,
+    "project_id": process.env.FIREBASE_project_id,
+    "private_key_id": process.env.FIREBASE_private_key_id,
+    "private_key": process.env.FIREBASE_private_key,
+    "client_email": process.env.FIREBASE_client_email,
+    "client_id": process.env.FIREBASE_client_id,
+    "auth_uri": process.env.FIREBASE_auth_uri,
+    "token_uri": process.env.FIREBASE_token_uri,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_auth_provider_x509_cert_url,
+    "client_x509_cert_url": process.env.FIREBASE_client_x509_cert_url,
+    "universe_domain": process.env.FIREBASE_universe_domain,
+}
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serverAccountInf)
 });
 
 
